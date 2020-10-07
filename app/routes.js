6b9409e3ad7b79e331501ae6f33a2bd6588fe1f9
Guy Tristram <guy.tristram@digital.trade.gov.uk>
2020-10-07 17:23:03 +0100

    links and states mostly there now.

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,21 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+
+router.post( '/signin', function (req, res)
+{
+    res.redirect('/verification?errorcode=false&');
+
+})
+
+router.post( '/verification', function (req, res)
+{
+
+    res.redirect('/failedverification');
+})
+
+router.post( '/failedverification', function (req, res)
+{
+    res.redirect('/signin');
+})
