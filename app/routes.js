ec38f887e88add5e9c0d6b2885b40a6a2042fbbf
Guy Tristram <guy.tristram@digital.trade.gov.uk>
2020-10-08 11:38:09 +0100

    updates to make functional

--- app/routes.js
+++ app/routes.js
@@ -8,15 +8,38 @@ module.exports = router
 
 router.post( '/signin', function (req, res)
 {
+    req.session.data['errorcounter'] == '0';
+    req.session.data['code'] == '';
     res.redirect('/verification?errorcode=false&');
-
 })
+
 
 router.post( '/verification', function (req, res)
 {
-
-    res.redirect('/failedverification');
+    if(req.session.data['code'] == '12345678' )
+    {
+        res.redirect('/failedverification?errorcode=false&');
+        req.session.data['errorcounter'] == '0';
+    }
+    else
+    {
+        if(req.session.data['errorcounter'] != '1'  &&  req.session.data['errorcounter'] != '2'  &&  req.session.data['errorcounter'] != '3')
+        {
+            req.session.data['errorcounter'] = '1'
+            res.redirect('/verification?errorcode=true&');
+        }
+        else if(req.session.data['errorcounter'] == '1' )
+        {
+            req.session.data['errorcounter'] = '2'
+            res.redirect('/verification?errorcode=true&');
+        }
+        else if(req.session.data['errorcounter'] == '2' )
+        {
+            res.redirect('/failedverification?errorcode=false&');
+        }
+    }
 })
+
 
 router.post( '/failedverification', function (req, res)
 {
